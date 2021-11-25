e811b861c0c62ce7b64cb0c347fb207a5e0cb0e1
arpit-maurya <arpitmaurya161@gmail.com>
2021-11-25 16:43:03 +0530

    myWhiteBoard

---
+++ app.js
@@ -0,0 +1,27 @@
+let express = require('express')
+let socket = require('socket.io')
+
+let app = express();
+app.use(express.static('public'))
+// http://localhost:5000
+let server = app.listen(5000, () => {
+ console.log('app.Listern is Working');
+})
+
+
+let io = socket(server);
+io.on('connection', (socket) => {
+
+  socket.on('beginPath', (data) => {
+      io.sockets.emit("beginPath",data)
+    })
+  socket.on('undoRedo', (data) => {
+      io.sockets.emit("undoRedo",data)
+    })
+  socket.on('drawStroke', (data) => {
+      io.sockets.emit("drawStroke",data)
+    })
+
+
+})
+
