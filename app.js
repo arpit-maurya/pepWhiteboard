539a7653dddb4079ff903c5bb48c5364279489e4
arpit-maurya <arpitmaurya161@gmail.com>
2021-11-25 18:03:43 +0530

    completed

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ let app = express();
 app.use(express.static('public'))
 // http://localhost:5000
 
-let port = process.env.PORT || 5000;
-let server = app.listen(port, () => {
+
+let server = app.listen(5000, () => {
  console.log('app.Listern is Working');
 })
 
