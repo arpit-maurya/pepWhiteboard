de800667135dd412d0aea5e8f0b0fe546d540604
arpit-maurya <arpitmaurya161@gmail.com>
2021-11-25 17:10:57 +0530

    update

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ let socket = require('socket.io')
 let app = express();
 app.use(express.static('public'))
 // http://localhost:5000
-let server = app.listen(5000, () => {
+
+let port = process.env.PORT || 5000;
+let server = app.listen(port, () => {
  console.log('app.Listern is Working');
 })
 
